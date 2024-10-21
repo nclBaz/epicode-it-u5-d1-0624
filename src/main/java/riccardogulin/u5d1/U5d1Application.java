c37ab5722138c28e8896664fde6eb27c57516be9
Riccardo Gulin <riccardo.gulin@gmail.com>
2024-10-21 11:12:18 +0200

    Adds Spring Boot project

---
+++ src/main/java/riccardogulin/u5d1/U5d1Application.java
@@ -0,0 +1,13 @@
+package riccardogulin.u5d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d1Application.class, args);
+	}
+
+}
